8476e103385a6c4cabd79504b784131334dea5c5
qibinyi <qibinyi@Qis-Mac-mini-6.local>
2022-10-29 18:23:55 +0800

    m

--- custom_gym/cust_gym_test.py
+++ custom_gym/cust_gym_test.py
@@ -1,12 +1,12 @@
 import gym
 import gym_examples
 
-env = gym.make("gym_examples/GridWorld-v0")
+env = gym.make("gym_examples/GridWorld-v0", render_mode="human")
 
 print(env.reset())
 
 # env.render()
 while True:
-    env.step(env.action_space.sample())  # take a random action
-    # env.step(0)
+    ob, *info = env.step(env.action_space.sample())  # take a random action
+    # print(ob, info)
     env.render()
